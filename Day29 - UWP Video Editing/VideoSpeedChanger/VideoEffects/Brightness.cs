e9ac5b35afa3c6f290f31a30283b625377e836f8
DosU <49920936+DosU13@users.noreply.github.com>
2023-12-07 08:20:14 +0600

    exploring uwp video editing

---
+++ Day29 - UWP Video Editing/VideoSpeedChanger/VideoEffects/Brightness.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using Windows.Foundation.Collections;
+using Windows.Graphics.DirectX.Direct3D11;
+using Windows.Media.Effects;
+using Windows.Media.MediaProperties;
+using Microsoft.Graphics.Canvas;
+using Microsoft.Graphics.Canvas.Effects;
+using System.Numerics;
+using System.Diagnostics;
+
+namespace VideoEffects
+{
+    public sealed class Brightness : IBasicVideoEffect
+    {
+        private Vector2 WhitePoint(float Intensity)
+        {
+            if (Intensity > 0) return new Vector2(1 - Intensity, 1);
+            else return new Vector2(1, 1 + Intensity);
+        }
+
+        public void ProcessFrame(ProcessVideoFrameContext context)
+        {
+            using (CanvasBitmap inputBitmap = CanvasBitmap.CreateFromDirect3D11Surface(_canvasDevice, context.InputFrame.Direct3DSurface))
+            using (CanvasRenderTarget renderTarget = CanvasRenderTarget.CreateFromDirect3D11Surface(_canvasDevice, context.OutputFrame.Direct3DSurface))
+            using (CanvasDrawingSession ds = renderTarget.CreateDrawingSession())
+            {
+                //TimeSpan time = context.InputFrame.RelativeTime.HasValue ? context.InputFrame.RelativeTime.Value : new TimeSpan();
+                //if (time.TotalSeconds < Start || time.TotalSeconds > End)
+                //{
+                //    ds.DrawImage(inputBitmap);
+                //    return;
+                //}
+
+                // START HERE
+                var brightness = new BrightnessEffect()
+                {
+                    Source = inputBitmap,
+                    BlackPoint = new Vector2(0, 0),
+                    WhitePoint = WhitePoint(0.5f)
+                };
+                ds.DrawImage(brightness);
+            }
+        }
+
+        //private Property Intensity;
+
+        //float Start;
+        //float End;
+        public void SetProperties(IPropertySet configuration)
+        {
+            //Start = Convert.ToSingle(configuration["Start"]);
+            //End = Convert.ToSingle(configuration["End"]);
+            ////START HERE
+            //Intensity = new Property(nameof(Intensity), configuration);
+        }
+
+        private CanvasDevice _canvasDevice;
+
+        public void SetEncodingProperties(VideoEncodingProperties encodingProperties, IDirect3DDevice device)
+        {
+            _canvasDevice = CanvasDevice.CreateFromDirect3D11Device(device);
+            CanvasDevice.DebugLevel = CanvasDebugLevel.Error;
+        }
+
+        public bool IsReadOnly { get { return false; } }
+        public MediaMemoryTypes SupportedMemoryTypes { get { return MediaMemoryTypes.Gpu; } }
+        public bool TimeIndependent { get { return false; } }
+
+        public IReadOnlyList<VideoEncodingProperties> SupportedEncodingProperties
+        {
+            get
+            {
+                return new List<VideoEncodingProperties>()
+                {
+                    VideoEncodingProperties.CreateUncompressed(MediaEncodingSubtypes.Argb32, 800, 600)
+                };
+            }
+        }
+
+        public void Close(MediaEffectClosedReason reason)
+        {
+            if (_canvasDevice != null)
+                _canvasDevice.Dispose();
+        }
+
+        public void DiscardQueuedFrames()
+        {
+        }
+    }
+}
---
+++ Day29 - UWP Video Editing/VideoSpeedChanger/VideoSpeedChanger/MainPage.xaml.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Media.Core;
+using Windows.Media.Editing;
+using Windows.Media.Effects;
+using Windows.Storage.Pickers;
+using Windows.Storage;
+using Windows.UI.Composition.Core;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Animation;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace VideoSpeedChanger
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public CompositionController compositionCrtl = new CompositionController();
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+            compositionCrtl.status = Status;
+            compositionCrtl.mediaPlayerElement = mediaPlayerElement;
+            compositionCrtl.ChangeRender += delegate (object sender, EventArgs arg)
+            {
+                UpdateMediaElementSource();
+            };
+        }
+
+        private MediaStreamSource mediaStreamSource;
+        public void UpdateMediaElementSource()
+        {
+            mediaStreamSource = compositionCrtl.composition.GeneratePreviewMediaStreamSource(
+                (int)mediaPlayerElement.ActualWidth,
+                (int)mediaPlayerElement.ActualHeight);
+            mediaPlayerElement.Source = MediaSource.CreateFromMediaStreamSource(mediaStreamSource);
+        }
+
+        private async void AddFile_Click(object sender, RoutedEventArgs e)
+        {
+            await PickFileAndAddClip();
+        }
+
+        private async void AddPhotos_Click(object sender, RoutedEventArgs e)
+        {
+            await PickPhotosAndAddToComp();
+        }
+
+        private async void AddEffect_Click(object sender, RoutedEventArgs e)
+        {
+            await AddEffect();
+        }
+
+        private async Task AddEffect()
+        {
+            var clip = compositionCrtl.composition.Clips.First();
+            clip.VideoEffectDefinitions.Add(new VideoEffectDefinition("VideoEffects.Brightness"));
+            compositionCrtl.Render();
+        }
+
+        private async Task PickFileAndAddClip()
+        {
+            var picker = new FileOpenPicker();
+            picker.SuggestedStartLocation = PickerLocationId.VideosLibrary;
+            picker.FileTypeFilter.Add(".mp4");
+            StorageFile pickedFile = await picker.PickSingleFileAsync();
+            if (pickedFile == null) return;
+
+            // These files could be picked from a location that we won't have access to later
+            var storageItemAccessList = Windows.Storage.AccessCache.StorageApplicationPermissions.FutureAccessList;
+            storageItemAccessList.Add(pickedFile);
+
+            var clip = await MediaClip.CreateFromFileAsync(pickedFile);
+
+            compositionCrtl.AddClip(clip);
+        }
+
+        private async Task PickPhotosAndAddToComp()
+        {
+            var picker = new FileOpenPicker();
+            picker.ViewMode = PickerViewMode.Thumbnail;
+            picker.SuggestedStartLocation = PickerLocationId.PicturesLibrary;
+            picker.FileTypeFilter.Add(".jpg");
+            picker.FileTypeFilter.Add(".jpeg");
+            picker.FileTypeFilter.Add(".png");
+
+            var files = await picker.PickMultipleFilesAsync();
+            if (files.Count > 0)
+            {
+                compositionCrtl.AddPhotos(files);
+            }
+        }
+    }
+}
